f1e5b51a586665a588d6371986caeeb3ce093d1b
Mateusz Rybczynski <mateusz.rybczynski@icloud.com>
2023-02-21 19:38:54 +0100

    Added opening csv files

--- search_engine.py
+++ search_engine.py
@@ -2,7 +2,7 @@
 import platform
 import os
 import time
-import pandas
+import pandas as pd
 import psycopg2
 from colorama import Fore, Back, Style
 
@@ -16,7 +16,7 @@ param_dic = {
 def connect(params):
     conn = None
     try: # Try to connect to database
-        print("Connecting to database")
+        print("\nConnecting to database")
         conn = psycopg2.connect(**params)
         print_center(Fore.GREEN+"Connection succesfull")
         print(Style.RESET_ALL)
@@ -48,7 +48,14 @@ def clear_screen():
 def main():
     conn = connect(param_dic)
     system_info()
+    phrases_df = pd.read_csv("phrases.csv") # Open file with not found phrases
     clear_screen()
     print_center("# # # #  H E L L O  # # # #")
+    compare_with_another_file = input("\nDo you want to compare with another file? [Y/N] ").upper()
+    if compare_with_another_file == "Y":
+        name_of_file = input("Name: ")
+        file_df = pd.read_csv(name_of_file+".csv") # Open file to compare
+    else:
+        print("Done")
 
 main()
