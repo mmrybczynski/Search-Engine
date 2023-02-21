495b511a0d8772e23e09ad93cac171fac9d7b777
Mateusz Rybczynski <mateusz.rybczynski@icloud.com>
2023-02-21 19:05:30 +0100

    Print center function

--- search_engine.py
+++ search_engine.py
@@ -3,12 +3,17 @@ import platform
 import os
 import time
 
+# Print center function
+def print_center(text):
+    print(text.center(os.get_terminal_size().columns))
+
 # Print info about system
 def system_info():
-    print("Processor: "+platform.processor())
-    print("System: "+platform.system()+" "+platform.release())
+    print("\nProcessor: "+platform.processor())
+    print("System: "+platform.system()+" "+platform.release()+"\n")
     time.sleep(3)
 
+# Clear screen
 def clear_screen():
     sys = str(platform.system()).upper()
     system_info()
@@ -19,4 +24,8 @@ def clear_screen():
         clear = lambda: os.system('clear')
         clear()
 
-clear_screen()
+def main():
+    clear_screen()
+    print_center("\n# # # #  H E L L O  # # # #\n")
+
+main()
