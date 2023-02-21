c826cbf75e510fdae6ee8f794f0d2c4f44460e76
Mateusz Rybczynski <mateusz.rybczynski@icloud.com>
2023-02-21 18:58:55 +0100

    Clear screen added

--- search_engine.py
+++ search_engine.py
@@ -7,5 +7,16 @@ import time
 def system_info():
     print("Processor: "+platform.processor())
     print("System: "+platform.system()+" "+platform.release())
+    time.sleep(3)
 
-system_info()
+def clear_screen():
+    sys = str(platform.system()).upper()
+    system_info()
+    if sys == "WINDOWS":
+        clear = lambda: os.system('cls')
+        clear()
+    else:
+        clear = lambda: os.system('clear')
+        clear()
+
+clear_screen()
